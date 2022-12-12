4c00080cacf5e6bfb5e2e3f66a9bf21fadc6b011
MadalinCernat <madalin.cernat02@gmail.com>
2022-12-12 11:08:44 +0200

    refactor

--- students_service.py
+++ students_service.py
@@ -1,6 +1,9 @@
+from repositories.base_repository import *
+from student import Student
 class StudentsService:
-    def __init__(self, repository):
+    def __init__(self, repository: BaseRepository):
         self._repository = repository
+        self.add_sample_data_memory()
 
     def delete_group_of_students(self, group):
         students = self._repository.get_all()
@@ -11,3 +14,30 @@ class StudentsService:
         # Return number of deleted students
         return len(students)
     
+    def add_student(self, student):
+        self._repository.add(student)
+    
+    def search(self, id):
+        return self._repository.find_by_id(id)
+    
+    def remove(self, id):
+        self._repository.remove_by_id(id)
+    
+    def remove_all(self, id):
+        self._repository.remove_all()
+
+    def get_all(self):
+        return self._repository.get_all()
+    
+    def undo(self, deleted_students):
+        self._repository.undo(deleted_students)
+
+    
+    def add_sample_data_memory(self):
+        self._repository.add(Student(1, "Student1", "911"))
+        self._repository.add(Student(2, "Student2", "911"))
+        self._repository.add(Student(3, "Student3", "912"))
+        self._repository.add(Student(4, "Student4", "912"))
+        self._repository.add(Student(5, "Student5", "912"))
+        self._repository.add(Student(6, "Student6", "914"))
+        self._repository.add(Student(7, "Student7", "914"))
--- ui.py
+++ ui.py
@@ -1,18 +1,67 @@
-from student import Student
+import json
+import ui
+from student import *
+from repositories.memory_repository import *
+from repositories.textfile_repository import *
+from students_service import *
 
-def print_menu():
-    print("Choose the operation you want to do:")
-    print("1. Add a student")
-    print("2. Display the list of students")
-    print("3. Delete a group of students")
-    print("4. Undo")
-    print("5. Exit")
 
-def read_student():
-    id = input("Student Id: ")
-    name = input("Student Name: ")
-    group = input("Student Group: ")
-    
-    student = Student(id, name, group)
-    return student
+class ui:
+    def main(self):
+        f = open("settings.json", "r")
+        settings = json.load(f)
+        f.close()
 
+        repoType = settings["RepositoryType"].lower()
+        repository = BaseRepository()
+
+        if repoType == "memory":
+            repository = MemoryRepository()
+        elif repoType == "textfile":
+            file_name = settings["FileName"]
+            repository = TextFileRepository(file_name)
+        
+        service = StudentsService(repository)
+        deleted_students = 1
+
+        while True:
+            self.print_menu()
+            option = int(input())
+            
+            if option == 1:
+                student = self.read_student()
+                try:
+                    service.add_student(student)
+                except ValueError as e:
+                    print(e)
+            elif option == 2:
+                students = service.get_all()
+                for stud in students:
+                    print(stud)
+            elif option == 3:
+                group = input("Enter group:")
+                deleted_students = service.delete_group_of_students(group)
+            elif option == 4:
+                service.undo(deleted_students)
+                deleted_students = 1
+            elif option == 5:
+                return
+
+    def print_menu(self):
+        print("Choose the operation you want to do:")
+        print("1. Add a student")
+        print("2. Display the list of students")
+        print("3. Delete a group of students")
+        print("4. Undo")
+        print("5. Exit")
+
+    def read_student(self):
+        id = input("Student Id: ")
+        name = input("Student Name: ")
+        group = input("Student Group: ")
+        
+        student = Student(id, name, group)
+        return student
+
+u = ui()
+u.main()
